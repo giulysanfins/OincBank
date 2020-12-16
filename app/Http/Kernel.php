6d412227737c03e58a3dcd242c6465c8010a6887
Guilherme Dakuzaku <makoto@yahp.com.br>
2020-12-16 13:39:43 -0300

    #2v4ruq

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'CheckPermission' => \App\Http\Middleware\CheckPermission::class,
+        'checkstatus' => \App\Http\Middleware\CheckStatus::class,
     ];
 }
